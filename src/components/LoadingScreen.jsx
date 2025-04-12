2e81d45cfb2b00632d47094d29596b935b4a0705
Jawad Ahmad <jawadahmad4804@gmail.com>
2025-04-12 16:44:27 +0500

    first commit

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 export const LoadingScreen = ({ onComplete }) => {
   const [text, setText] = useState("");
-  const fullText = "<Hello Wolrd />";
+  const fullText = "<Hey, Folks! 👋 />";
 
   useEffect(() => {
     let index = 0;
